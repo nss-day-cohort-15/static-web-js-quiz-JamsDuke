80fb50d62548b643310ad60b92c04ec70ba8f22f
Jamie Duke <jamsduke@gmail.com>
2016-07-25 14:34:19 -0500

    slanted tree!

--- quiz.js
+++ quiz.js
@@ -2,13 +2,13 @@ var userHeight = document.getElementById("userHeight");
 var userLeaf = document.getElementById("userLeaf");
 var button = document.getElementById("treeGrown");
 button.addEventListener("click", growTree);
-var spacer = '';
+var spacer = 1;
 
 function growTree (clickEvent) {
   if (userLeaf.value === "" || userHeight.value === "")
     alert("You must specify a height and tree leaf!")
   for (var i = 0; i < userHeight.value; i++){
-    console.log(userLeaf.value);
+    console.log("".repeat(userHeight.value - i) + userLeaf.value.repeat(spacer));
     spacer += 2;
   }
 }
