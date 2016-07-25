baf806bf88eae6e61ddde1d41d7b6192db8bf1e4
Jamie Duke <jamsduke@gmail.com>
2016-07-25 15:27:02 -0500

    tree now grows!

--- quiz.js
+++ quiz.js
@@ -2,14 +2,14 @@ var userHeight = document.getElementById("userHeight");
 var userLeaf = document.getElementById("userLeaf");
 var button = document.getElementById("treeGrown");
 button.addEventListener("click", growTree);
-var spacer = 1;
+var growth = 1;
 
 function growTree (clickEvent) {
   if (userLeaf.value === "" || userHeight.value === "")
     alert("You must specify a height and tree leaf!")
   for (var i = 0; i < userHeight.value; i++){
-    console.log("".repeat(userHeight.value - i) + userLeaf.value.repeat(spacer));
-    spacer += 2;
+    console.log(" ".repeat(userHeight.value-i) + userLeaf.value.repeat(growth));
+    growth += 2;
   }
 }
 
